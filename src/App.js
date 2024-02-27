42e7de2cfed8eb823890fabf3903005d32c39971
Jansen Gabrillo <Gabrillojansen@gmail.com>
2024-02-27 22:41:32 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Collections } from './components/Collections';
 import { Header } from './components/Header';
 import { Banner } from './components/Banner';
